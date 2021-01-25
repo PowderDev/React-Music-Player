95de9a9c9f6793e345c6ad148872f0e853b10f14
Timyr <timyrforwork@gmail.com>
2021-01-25 21:16:46 +0300

    'Music'

--- src/App.js
+++ src/App.js
@@ -5,20 +5,15 @@ import chillHop from './data';
 import Library from './components/library';
 import Nav from './components/nav';
 import './styles/app.css';
-import axios from 'axios';
-
+import axios from 'axios'
 
 
 
 
 
  function App() {
-
-
-
   const audioRef = useRef(null)
-  let [songs, setSongs] = useState(chillHop());
-
+  const [songs, setSongs] = useState([]);
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
   const [songInfo, setSongInfo] = useState({
@@ -28,7 +23,6 @@ import axios from 'axios';
         open: false,
         animationPercentage: 0 
     });
-  const {audio} = currentSong;
   const [libraryStatus, setLibraryStatus] = useState(false)
 
 
@@ -50,14 +44,33 @@ import axios from 'axios';
       if(isPlaying) audioRef.current.play()
     }
 
-  return (
-    <div className={`App ${libraryStatus ? 'library-active' : ''} `}>
-      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus}/>
-      <Song currentSong={currentSong} isPlaying={isPlaying} libraryStatus={libraryStatus}/>
-      <Player libraryStatus={libraryStatus} setSongs={setSongs} setCurrentSong={setCurrentSong} songs={songs} currentSong={currentSong} setIsPlaying={setIsPlaying} isPlaying={isPlaying} songInfo={songInfo} setSongInfo={setSongInfo} audioRef={audioRef}/>
-      <Library songs={songs} setCurrentSong={setCurrentSong} audioRef={audioRef} songInfo={songInfo} isPlaying={isPlaying} setIsPlaying={setIsPlaying} setSongs={setSongs} libraryStatus={libraryStatus}/>
-      <audio onEnded={songEnded} onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} ref={audioRef} src={audio}></audio>
-    </div>
+
+
+  useEffect(() => {
+    axios('https://react-music-player-dc6f6-default-rtdb.firebaseio.com/songs.json')
+      .then(res => {
+        setSongs(Object.values(res.data)[0])
+        setCurrentSong(Object.values(res.data)[0][0])
+      })
+      .catch((e) => console.log(e))
+  }, [])
+
+
+  return(
+    
+      <div className={`App ${libraryStatus ? 'library-active' : ''} `}>
+        {songs.length !== 0 && (
+          <>
+            <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus}/>
+            <Song currentSong={!currentSong ? songs[0] : currentSong} isPlaying={isPlaying} libraryStatus={libraryStatus}/>
+            <Player libraryStatus={libraryStatus} setSongs={setSongs} setCurrentSong={setCurrentSong} songs={songs} currentSong={!currentSong ? songs[0] : currentSong} setIsPlaying={setIsPlaying} isPlaying={isPlaying} songInfo={songInfo} setSongInfo={setSongInfo} audioRef={audioRef}/>
+            <Library songs={songs} setCurrentSong={setCurrentSong} audioRef={audioRef} songInfo={songInfo} isPlaying={isPlaying} setIsPlaying={setIsPlaying} setSongs={setSongs} libraryStatus={libraryStatus}/>
+            <audio onEnded={songEnded} onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} ref={audioRef} src={!currentSong ? songs[0].audio : currentSong.audio}></audio>
+          </>
+        )}
+
+      </div>
+    
   );
 }
 
